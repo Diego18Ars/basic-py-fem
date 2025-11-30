d1e4aa2bf8c0046b325379b1365b038ea1833c17
Diego18Ars <82908015+Diego18Ars@users.noreply.github.com>
2025-11-30 22:05:23 +0000

    contraint_dof rewritten
    Now it needs an optional parameter called penalty (which will be automatized eventually), and contrained DoFs have to be passed individually

--- boundary_conditions.py
+++ boundary_conditions.py
@@ -1,10 +1,25 @@
-import math as m
 import numpy as np
 
-from elements import Node, Bar
+from elements import Node
 
-def constraint_dof(dofs: tuple, value: float, stiff: np.array, loads: np.array):
+def constrain_dof(node: Node, nodal_dof: int, value: float, K: np.array, F: np.array, penalty):
+    '''Contrain Degree of Freedom for Node. \n
+    node - Node class object that indicates the node to be affected by the constraint \n
+    nodal_dof - Index integer indicating which DoF is to be constrained \n
+        1 - X translation \n
+        2 - Y translation \n
+        3 - Rotations \n
+    value - Float indicating the displacement value on the input node to be constrained
+    '''
+    global_dof = node.dofs[nodal_dof-1] # Ready for 0-indexing arrays
+    K[global_dof][global_dof] += penalty
+    F[global_dof] += value*penalty
+
+def couple_dofs(dofs: tuple, stiff: np.array):
     penalty = np.max(stiff) * 10**4
-    for dof in dofs:
-        stiff[dof-1][dof-1] += penalty
-        loads[dof-1] += value*penalty
+    for dof1 in dofs:
+        for dof2 in dofs:
+            if dof1 == dof2:
+                stiff[dof1-1][dof2-1] += penalty
+            else: 
+                stiff[dof1-1][dof2-1] -= penalty
